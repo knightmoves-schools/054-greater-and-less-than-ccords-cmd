07b94ff916cca511d63cb9872d91f99ef487526d
ccords-cmd <ccords@anamosa.k12.ia.us>
2025-11-18 17:05:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-
+function oldEnough(age) {
+  if (age > 17) {
+    return document.getElementById('result').innerHTML = 'old enough';
+  }else{
+    document.getElementById('result').innerHTML = 'not old enough';
+  };
+};
+function underAge(age) {
+  if (age < 17) {
+    return document.getElementById('result').innerHTML = 'under age';
+  }else{
+    document.getElementById('result').innerHTML = 'not under age';
+  };
+};
+function legalSpeed(speed) {
+  if (speed <= 50) {
+    return document.getElementById('result').innerHTML = 'legal speed';
+  };
+};
+function isGoldenYears(age) {
+  if (age >= 65) {
+    return document.getElementById('result').innerHTML = 'golden years';
+  };
+};
